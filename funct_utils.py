000e1d4ac3119518e42755bdb70398c6e0b2190a
dave <bianchini.davide.89@gmail.com>
2017-04-14 11:30:07 +0200

    adding custom integrator

--- funct_utils.py
+++ funct_utils.py
@@ -1,20 +1,22 @@
 
 # coding: utf-8
 
-# In[11]:
+# In[1]:
 
 from math import *
 
 
-# In[7]:
+# In[2]:
 
 def toPythonPower(string):
     return string.replace("^","**")
 
 
-# In[8]:
+# In[26]:
 
+possible_args=['y','z']
 def stringToFunction(string):
+    string = ''.join([c if c not in possible_args else 'x' for c in string])
     def func(x):
         return eval(toPythonPower(string))
     return func
